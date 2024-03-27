41208d3552d1638d7a625ddf03e51bc21c84a655
esra <esraadwidar770@gmail.com>
2024-03-27 03:16:22 +0200

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+//var dbtn = document.querySelector(".dbtn");
+var btn = document.querySelector(".btn");
+btn.addEventListener("click", function(){
+   // dbtn.innerHTML="done";
+   console.log("mmm");
+})
